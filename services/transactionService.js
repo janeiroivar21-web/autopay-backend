417d0bc209d84fcc948f22ae717519ee88233f8f
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:47:59 +0300

    Update transactionService.js

--- services/transactionService.js
+++ services/transactionService.js
@@ -1,1 +1,23 @@
+const { transactions } = require("./firestoreService");
 
+/*
+=========================================
+SAVE TRANSACTION
+=========================================
+*/
+
+async function saveTransaction(data) {
+
+    await transactions.add({
+
+        ...data,
+
+        createdAt: new Date()
+
+    });
+
+}
+
+module.exports = {
+    saveTransaction
+};
