8a7d539aa18abe143222dfd0068a0dfbcebfc594
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:54:19 +0300

    Update withdraw.js

--- routes/withdraw.js
+++ routes/withdraw.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 
+const { withdraw } = require("../controllers/withdrawController");
+
+// POST /api/withdraw
+router.post("/", withdraw);
+
+module.exports = router;
