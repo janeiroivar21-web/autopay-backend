d2ec7b0e62306f01aaa68ca9c22098d067d4b2c8
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:53:48 +0300

    Update status.js

--- routes/status.js
+++ routes/status.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 
+const { checkStatus } = require("../controllers/statusController");
+
+// POST /api/check-status
+router.post("/", checkStatus);
+
+module.exports = router;
