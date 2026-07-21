2e1fff8086f980cf689df5b85d2b3367bd17185c
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:48:27 +0300

    Update withdrawalService.js

--- services/withdrawalService.js
+++ services/withdrawalService.js
@@ -1,1 +1,23 @@
+const { withdrawals } = require("./firestoreService");
 
+/*
+=========================================
+SAVE WITHDRAWAL
+=========================================
+*/
+
+async function saveWithdrawal(data) {
+
+    await withdrawals.add({
+
+        ...data,
+
+        createdAt: new Date()
+
+    });
+
+}
+
+module.exports = {
+    saveWithdrawal
+};
