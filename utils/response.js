b6b8ef29460ed2af11c05c60682338f8e4b48d8d
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:41:17 +0300

    Update response.js

--- utils/response.js
+++ utils/response.js
@@ -1,1 +1,23 @@
+function success(res, message, data = {}) {
 
+    return res.json({
+        success: true,
+        message,
+        ...data
+    });
+
+}
+
+function error(res, message, code = 500) {
+
+    return res.status(code).json({
+        success: false,
+        message
+    });
+
+}
+
+module.exports = {
+    success,
+    error
+};
