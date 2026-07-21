0da78e7424ff7152164cf4c83f20c954c23563c6
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:46:02 +0300

    Update merchantService.js

--- services/merchantService.js
+++ services/merchantService.js
@@ -1,1 +1,51 @@
+const { users } = require("./firestoreService");
 
+/*
+=========================================
+GET MERCHANT
+=========================================
+*/
+
+async function getMerchant(uid) {
+
+    const doc = await users.doc(uid).get();
+
+    if (!doc.exists) {
+        return null;
+    }
+
+    return {
+        id: doc.id,
+        ...doc.data()
+    };
+}
+
+/*
+=========================================
+UPDATE MERCHANT
+=========================================
+*/
+
+async function updateMerchant(uid, data) {
+
+    await users.doc(uid).update(data);
+
+}
+
+/*
+=========================================
+CREATE MERCHANT
+=========================================
+*/
+
+async function createMerchant(uid, data) {
+
+    await users.doc(uid).set(data);
+
+}
+
+module.exports = {
+    getMerchant,
+    updateMerchant,
+    createMerchant
+};
