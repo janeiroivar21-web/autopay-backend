f235183f11fb6dd717e3820670ef7a2c2f0271c6
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 22:23:34 +0300

    Update merchant.js

--- routes/merchant.js
+++ routes/merchant.js
@@ -3,13 +3,33 @@ const router = express.Router();
 
 const {
     getMerchant,
-    updateMerchant
+    updateMerchant,
+    createPaymentAccount,
+    getPaymentAccounts
 } = require("../controllers/merchantController");
+
+/*
+=========================================
+MERCHANT PROFILE
+=========================================
+*/
 
 // GET /api/merchant/:uid
 router.get("/:uid", getMerchant);
 
 // PUT /api/merchant/:uid
 router.put("/:uid", updateMerchant);
+
+/*
+=========================================
+PAYMENT ACCOUNTS
+=========================================
+*/
+
+// POST /api/merchant/account
+router.post("/account", createPaymentAccount);
+
+// GET /api/merchant/accounts/:uid
+router.get("/accounts/:uid", getPaymentAccounts);
 
 module.exports = router;
