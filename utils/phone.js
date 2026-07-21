839068531bd47750cc4fcaf64eb179f62f5266e4
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:39:45 +0300

    Update phone.js

--- utils/phone.js
+++ utils/phone.js
@@ -1,1 +1,26 @@
+function formatPhone(phone) {
 
+    phone = phone.toString().trim();
+
+    if (phone.startsWith("+254")) {
+        return phone.replace("+", "");
+    }
+
+    if (phone.startsWith("254")) {
+        return phone;
+    }
+
+    if (phone.startsWith("07")) {
+        return "254" + phone.substring(1);
+    }
+
+    if (phone.startsWith("01")) {
+        return "254" + phone.substring(1);
+    }
+
+    return phone;
+}
+
+module.exports = {
+    formatPhone
+};
