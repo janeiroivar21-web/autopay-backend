0cb97544e0b7a662ef92a6660248387e441f36c7
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:46:33 +0300

    Update firestoreService.js

--- services/firestoreService.js
+++ services/firestoreService.js
@@ -1,1 +1,22 @@
+const { db } = require("../config/firebase");
 
+/*
+=========================================
+FIRESTORE COLLECTIONS
+=========================================
+*/
+
+const users = db.collection("users");
+const transactions = db.collection("transactions");
+const withdrawals = db.collection("withdrawals");
+const paymentAccounts = db.collection("paymentAccounts");
+const paymentLinks = db.collection("paymentLinks");
+
+module.exports = {
+    db,
+    users,
+    transactions,
+    withdrawals,
+    paymentAccounts,
+    paymentLinks
+};
