0b5e1c1a3621f9e4754c3ae287b17aa24ac621b3
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:40:41 +0300

    Update reference.js

--- utils/reference.js
+++ utils/reference.js
@@ -1,1 +1,11 @@
+const { v4: uuid } = require("uuid");
 
+function generateReference(prefix = "TXN") {
+
+    return `${prefix}_${Date.now()}_${uuid().substring(0,8)}`;
+
+}
+
+module.exports = {
+    generateReference
+};
