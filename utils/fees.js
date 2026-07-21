45a653ef991cfbb948c72818f9e62d6abc4cfc21
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:40:19 +0300

    Update fees.js

--- utils/fees.js
+++ utils/fees.js
@@ -1,1 +1,39 @@
+/*
+=================================
+AUTOPAY FEES
+=================================
+*/
 
+const TRANSACTION_FEE = 0.08;
+const WITHDRAW_FEE = 0.10;
+
+function calculateTransactionFee(amount) {
+
+    amount = Number(amount);
+
+    const fee = amount * TRANSACTION_FEE;
+
+    return {
+        amount,
+        fee,
+        merchantReceives: amount - fee
+    };
+}
+
+function calculateWithdrawFee(amount) {
+
+    amount = Number(amount);
+
+    const fee = amount * WITHDRAW_FEE;
+
+    return {
+        amount,
+        fee,
+        merchantReceives: amount - fee
+    };
+}
+
+module.exports = {
+    calculateTransactionFee,
+    calculateWithdrawFee
+};
