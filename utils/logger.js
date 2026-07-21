d9fcc6e4c56a9ce85acc335e7a4ac3e9cb6728f5
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-21 21:41:42 +0300

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,22 @@
+function log(title, data = "") {
 
+    console.log(
+        `[${new Date().toISOString()}] ${title}`,
+        data
+    );
+
+}
+
+function error(title, data = "") {
+
+    console.error(
+        `[${new Date().toISOString()}] ${title}`,
+        data
+    );
+
+}
+
+module.exports = {
+    log,
+    error
+};
