0ac546bd88af0ccdd729d1fc9449c9ec704363ca
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-23 15:25:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const { stkPush: sendStk } = require("./services/swiftService");
 
 const app = express();
 
