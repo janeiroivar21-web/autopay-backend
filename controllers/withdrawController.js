7f39ccce94cc6823b2b2188dc0c6c45b854cadea
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-23 11:01:35 +0300

    Update withdrawController.js

--- controllers/withdrawController.js
+++ controllers/withdrawController.js
@@ -1,5 +1,5 @@
-const optimaService = require("../services/optimaService");
-const withdrawalService = require("../services/withdrawalService");
+const swiftService = require("../services/swiftService");
+const walletService = require("../services/walletService");
 const { calculateWithdrawFee } = require("../utils/fees");
 const { generateReference } = require("../utils/reference");
 const { success, error } = require("../utils/response");
@@ -20,25 +20,25 @@ async function withdraw(req, res) {
 
         }
 
-        const fee = calculateWithdrawFee(amount);
+        const withdrawalFee = Number(amount) * 0.10;
+
+const payoutAmount = Number(amount) - withdrawalFee;
 
         const reference = generateReference("WD");
 
-        const payout = await optimaService.payout(
-
-            phone,
-            fee.merchantReceives,
-            reference
-
-        );
+        const payout = await swiftService.payout(
+    phone,
+    payoutAmount,
+    reference
+);
 
         await withdrawalService.saveWithdrawal({
 
             uid,
             phone,
             amount,
-            fee: fee.fee,
-            paid: fee.merchantReceives,
+            fee: withdrawalFee,
+            paid: payoutAmount,
             reference,
             status: "Pending"
 
