495968ab063cbe2ebf7a3af435e99cbd762f1583
janeiroivar21-web <janeiroivar21@gmail.com>
2026-07-25 22:14:04 +0300

    Update optimaService.js

--- services/optimaService.js
+++ services/optimaService.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 const { formatPhone } = require("../utils/phone");
-const { log, error } = require("../utils/logger");
+const { log } = require("../utils/logger");
 
 const API_BASE = "https://optimapaybridge.co.ke/api/v2";
 
@@ -23,44 +23,86 @@ async function stkPush(
     reference = null,
     customerName = "AUTOPAY Customer"
 ) {
-
     try {
 
         const payload = {
-
             payment_account_id: Number(process.env.OPTIMA_PAYMENT_ACCOUNT_ID),
-
             phone: formatPhone(phone),
-
             amount: Math.round(Number(amount)),
-
             reference: reference || `AUTO-${Date.now()}`,
-
             description: customerName
-
         };
 
         log("Sending OptimaPay STK Push", payload);
 
         const response = await axios.post(
-
             `${API_BASE}/stkpush.php`,
-
             payload,
-
             {
                 headers: HEADERS
             }
-
         );
 
-        log("OptimaPay Response", response.data);
+        log("OptimaPay STK Response", response.data);
 
         return response.data;
 
     } catch (err) {
 
-        const errorLog = `
+        saveError(err);
+
+        throw err;
+
+    }
+}
+
+/*
+=========================================
+CHECK PAYMENT STATUS
+=========================================
+*/
+
+async function checkStatus(checkoutRequestId) {
+
+    try {
+
+        const payload = {
+            checkout_request_id: checkoutRequestId
+        };
+
+        log("Checking OptimaPay Status", payload);
+
+        const response = await axios.post(
+            `${API_BASE}/status.php`,
+            payload,
+            {
+                headers: HEADERS
+            }
+        );
+
+        log("OptimaPay Status Response", response.data);
+
+        return response.data;
+
+    } catch (err) {
+
+        saveError(err);
+
+        throw err;
+
+    }
+
+}
+
+/*
+=========================================
+ERROR LOGGER
+=========================================
+*/
+
+function saveError(err) {
+
+    const errorLog = `
 ==============================
 ${new Date().toISOString()}
 
@@ -75,16 +117,16 @@ ${JSON.stringify(err.response?.data, null, 2)}
 
 `;
 
-        fs.appendFileSync("optimapay-error.log", errorLog);
+    fs.appendFileSync(
+        "optimapay-error.log",
+        errorLog
+    );
 
-        console.error(errorLog);
-
-        throw err;
-
-    }
+    console.error(errorLog);
 
 }
 
 module.exports = {
-    stkPush
+    stkPush,
+    checkStatus
 };
